c9cfc7c71b087522f6fb34675fc65a378090597b
NimbusEliott <105793091+NimbusEliott@users.noreply.github.com>
2022-06-20 20:16:30 +0200

    Add files via upload

--- SAE23-Rendu/Page_Gestion.php
+++ SAE23-Rendu/Page_Gestion.php
@@ -23,7 +23,7 @@
         </nav>
 <?php
 
-    //---------------------database connection---------------------
+    //---------------------connexion a la base de donnee---------------------
     $servername = "localhost";
     $username = "eliott";
     $password = "bonjour";
@@ -40,31 +40,32 @@
     }
 ?>
 
-<form method="POST" action="Page_affichage.php"/>
+<form method="POST" action="Page_Affichage.php"/>
 
 <?php
-    //----------------------Recovery of the room by manager-------------
+    //----------------------Recuperation de la salle par gestionnaire-------------
     $query_salle = "SELECT DISTINCT `capteur`.`salle`
         FROM `capteur`
         INNER JOIN `batiment` ON `batiment`.`id_bat` = `capteur`.`batiment`
-        WHERE `batiment`.`id_bat` = '1'"; //request to retrieve the rooms on the database according to the manager to connect
-    $result_salle = mysqli_query($conn, $query_salle); //execute sql query to database
-    $nb_salle = mysqli_num_rows($result_salle); //variable with the number of rooms
-    for ($i = 0; $i <= $nb_salle-1; $i++){ //loop to create as many sections as there are rooms
+        WHERE `batiment`.`id_bat` = '1'"; //requete pour récupérer les salles sur la base de donnée en fonction du gestionnaire connecter
+    $result_salle = mysqli_query($conn, $query_salle); //execution de la requete sql vers la base de donnée
+    $nb_salle = mysqli_num_rows($result_salle); //variable avec le nombre de salle
+    for ($i = 0; $i <= $nb_salle-1; $i++){ //boucle pour crée autant de rubrique qu'il n'y  a de salle
         $j = 0;
         while($row = mysqli_fetch_array($result_salle)) {
             $salle[$i] = $row[0];
             echo "<fieldset><legend>$salle[$i]</legend>
                     <label for='date$j'>Le </label>
-                    <input type='date' min='2022-06-17' name='date$j'>
+                    <input type='date' min='2022-06-15' name='date$j'>
                     <label for='heure1_$j'>entre </label>
                     <input type='time' name='heure1_$j'/>
                     <label for='heure2_$j'>et </label>
-                    <input type='time' name='heure2_$j'/></fieldset>"; //generation of the forms according to the manager and the number of rooms
+                    <input type='time' name='heure2_$j'/></fieldset>"; //génération du formulaires en fonction du gestionnaire et du nombre de salle
             $j++;
         }
     }
 ?>
+<input type="submit" name="submit" value="Valider" />
 </form>
 </body>
 </html>
