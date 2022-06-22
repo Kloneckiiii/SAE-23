abd9119369f6132d4887c95c6fc3f777c7706e43
The-MaxDarKk <105793149+The-MaxDarKk@users.noreply.github.com>
2022-06-22 11:41:46 +0200

    Add files via upload

---
+++ scripts_php/page_gest.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <?php        
+
+?>
+    <title>Page pour les gestionnaires</title>
+  </head>
+  <h1><align cen> Bienvenue sur la page dedié aux gestionnaires</h1>
+<body>
+<form name="insertion" action="" method="POST"
+      <td colspan="2"><input type="submit" value="insérer"></td>
+    </tr>
+  </table>
+</form>
+</body>
+</html>
